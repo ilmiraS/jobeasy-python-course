7f4c70a66ac9501dc5d61b33244e9081a4cc96fb
iandroni <iandroni@mail.ccsf.edu>
2020-08-19 01:50:36 -0700

    Homework 2

--- lesson_1/homework_1_6.py
+++ lesson_1/homework_1_6.py
@@ -1,7 +1,10 @@
-# Write your first program. Enter the temperature right now in Fahrenheit in temperature_fahrenheit variable as
-# a string (e.g. '75') and convert it to Celsius.
-# !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
+# Write your first proger the temperature right now in Fahrenheit in temperature_fahrenheit variable as
+# # a string (e.g. '75') and convert it to Celsius.
+# # !important you should save only number to result_temperature. Formula (32°F − 32) × 5/9 = 0°C
+#
+# # type your code here
 
-# type your code here
-temperature_fahrenheit = None
-result_temperature = None
+#temperature_fahrenheit = input("Enter temp in F:  ")
+temperature_fahrenheit = 75
+result_temperature = (temperature_fahrenheit - 32) * 5/9
+print(int(result_temperature))
